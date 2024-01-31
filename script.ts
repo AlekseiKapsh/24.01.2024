36b559eaa0942c4786f618216f69a64b620007bd
Капшиенко Алексей <AlexseiKapsh@stud.vvsu.ru>
2024-01-31 18:04:49 +1000

    First

---
+++ script.ts
@@ -0,0 +1,32 @@
+// const c:string[] = ["a","b","c"];
+// const c:any[] = ["a","b","c"];
+// const b:number = 1;
+// const n:string= "hello";
+// const arrow: () => void = () =>{};
+
+
+class classNmae{}
+class Point{x: number; y: number;}
+let point : Point = new Point();
+
+function func1(){
+    point.x = 1;
+    point.y = 2;
+};
+
+console.log(point);
+
+class Point2{
+    height: number;
+    width: number;
+};
+let point2 : Point2 = new Point2();
+function func2(){
+    point2.height = 1;
+    point2.width = 2;
+};
+
+console.log(point2);
+
+
+
